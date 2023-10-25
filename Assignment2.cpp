06a4d78e4eab78d852c18a86c5e5d741321ac45a
Danish-17 <90982444+Danish-17@users.noreply.github.com>
2023-10-26 00:27:44 +0500

    Add files via upload

---
+++ Assignment2.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+#include "Header1.h"
+
+
+using namespace std;
+
+
+
+class Stack {
+private:
+    Node* top;
+
+public:
+    Stack() {
+        top = nullptr;
+    }
+
+    ~Stack() {
+        Node* temp;
+        while (top != nullptr) {
+            temp = top;
+            top = top->next;
+            delete temp;
+        }
+    }
+
+    void push(char c) {
+        Node* newNode = new Node;
+        newNode->data = c;
+        newNode->next = top;
+        top = newNode;
+    }
+
+    char pop() {
+        if (top == nullptr) {
+            cout << "Stack underflow" << endl;
+            return '\0';
+        }
+        char c = top->data;
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+        return c;
+    }
+
+    bool isEmpty() {
+        return (top == nullptr);
+    }
+};
+
+void reverseString(string& str) {
+    int n = str.length();
+    Stack stack;
+
+
+    for (int i = 0; i < n; i++) {
+        stack.push(str[i]);
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        str[i] = stack.pop();
+    }
+}
+
+int main() {
+    string str = "Hey! There How are you?";
+    cout << "Original String: " << str << endl;
+    reverseString(str);
+    cout << "Reversed String: " << str << endl;
+
+    return 0;
+}
